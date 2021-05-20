e2f7d3a3ed1799b44613bec857701afc6b8d5ec4
Charlene CAESAR Lab <q2330416@gmail.com>
2021-05-20 15:28:26 +0800

    Cleaning and data sorting

--- Export_gct_cls_To_GSEA_TCGA_Pan_Filter.R
+++ Export_gct_cls_To_GSEA_TCGA_Pan_Filter.R
@@ -1,45 +1,50 @@
-## Clear variables
+## Clean variables
 rm(list = ls())
 
-## Initialization
-
+######## Path setting ########
 setwd(getwd()) ## Set current working directory
 PathName <- getwd() ## Set output directroy
 
-FileName <- c("Xena_TCGA_PanCancer_GE.xena")
-FileName2 <- c("Xena_TCGA_PanCancer_Pheno")
-FileName3 <- c("Xena_TCGA_PanCancer_Pheno2.tsv")
+FileName <-  c("Xena_TCGA_Pan")
+FileNameGE <- c("Xena_TCGA_PanCancer_GE.xena")
+FileNamePheno1 <- c("Xena_TCGA_PanCancer_Pheno")
+FileNamePheno2 <- c("Xena_TCGA_PanCancer_Pheno2.tsv")
 
-Target_gene_name_Multi <- c("RHOA_related_StageI&II(ajcc)")
+Target_gene_name_Multi <- c("RHOA_Comb1_FC1p2_AjccSTGI&II")
+# Comb:Combination # FC:Fold_Change # STG:Stage
+
 Target_gene_name <- c("RHOA","ROCK1","ROCK2","FN1")
-SetVersion <- c("_20210519V5")
+SetVersion <- c("_20210520")
 
 ResultFolderName <- paste0("/",Target_gene_name_Multi,SetVersion) ## Generate output folder automatically
 dir.create(paste0(PathName,ResultFolderName))
 
-## Import genetic data file
-GeneExp_Ori <- read.delim(paste0(PathName,"/",FileName),  # 資料檔名
+######## Parameter setting ########
+Fold_Change = 1.2
+
+######## Import Raw Data ########
+
+##Import gene expression data
+GeneExp_Ori <- read.delim(paste0(PathName,"/",FileNameGE),  # 資料檔名
                           header=T,          # 資料中的第一列，作為欄位名稱
                           sep="\t")
-# GeneExp_Ori <- read.table(paste0(PathName,"/Xena_TCGA_LGG_GE"),  # 資料檔名
-#                           header=F,          # 資料中的第一列，作為欄位名稱
-#                           sep="")
 
-## Import phenotype data file
-Phenotype_Ori <- read.delim(paste0(PathName,"/",FileName2),  # 資料檔名
+## Import phenotype data
+Phenotype1_Ori <- read.delim(paste0(PathName,"/",FileNamePheno1),  # 資料檔名
                             header=T,          # 資料中的第一列，作為欄位名稱
                             sep="\t")
 
-Phenotype_Ori2 <- read.delim(paste0(PathName,"/",FileName3),  # 資料檔名
+Phenotype2_Ori <- read.delim(paste0(PathName,"/",FileNamePheno2),  # 資料檔名
                             header=T,          # 資料中的第一列，作為欄位名稱
                             sep="\t")
 
-###################################### Note ######################################
+#*************************************** Note ***************************************#
 # paste0 ==> concatenate strings without any separation/delimiter
 # paste("Hello", "World", sep = "-") ==> concatenate strings with seperator "-"
-##################################################################################
+#*************************************** Note ***************************************#
 
 
+######## Data sorting ########
 GeneExp <- GeneExp_Ori 
 GeneExp[,1] <- as.character(GeneExp[,1])
 
@@ -59,13 +64,13 @@ GeneExp <- GeneExp[, -1]
 # GeneExp <- GeneExp[-1, ]
 
 ######## Filter Data by Phenotype ########
-Phenotype_Stage <-  Phenotype_Ori[Phenotype_Ori$clinical_stage %in% c("Stage I","Stage II"),]
-Phenotype_Stage <-  Phenotype_Ori[Phenotype_Ori$ajcc_pathologic_tumor_stage %in% c("Stage I","Stage II"),]
+Phenotype_Stage <-  Phenotype1_Ori[Phenotype1_Ori$clinical_stage %in% c("Stage I","Stage II"),]
+Phenotype_Stage <-  Phenotype1_Ori[Phenotype1_Ori$ajcc_pathologic_tumor_stage %in% c("Stage I","Stage II"),]
 Phenotype_Stage2 <- Phenotype_Stage
 Phenotype_Stage2$X_PATIENT <- gsub(pattern = '-',replacement = '.',x = Phenotype_Stage2$X_PATIENT)
 Phenotype_Stage2$sample <- gsub(pattern = '-',replacement = '.',x = Phenotype_Stage2$sample)
 
-Phenotype_Stage_All <- Phenotype_Ori
+Phenotype_Stage_All <- Phenotype1_Ori
 # Try stage
 Phenotype_Stage_All <- Phenotype_Stage2 
 Phenotype_Stage_All$X_PATIENT <- gsub(pattern = '-',replacement = '.',x = Phenotype_Stage_All$X_PATIENT)
@@ -74,11 +79,11 @@ Phenotype_Stage_All$sample <- gsub(pattern = '-',replacement = '.',x = Phenotype
 
 PT = "Primary Tumor"
 STN = "Solid Tissue Normal"
-Phenotype_PT <-  Phenotype_Ori2[Phenotype_Ori2$sample_type %in% PT,]
+Phenotype_PT <-  Phenotype2_Ori[Phenotype2_Ori$sample_type %in% PT,]
 Phenotype_PT2 <- Phenotype_PT
 Phenotype_PT2$sample <- gsub(pattern = '-',replacement = '.',x = Phenotype_PT2$sample)
 
-Phenotype_STN <-  Phenotype_Ori2[Phenotype_Ori2$sample_type %in% STN,]
+Phenotype_STN <-  Phenotype2_Ori[Phenotype2_Ori$sample_type %in% STN,]
 Phenotype_STN2 <- Phenotype_STN
 Phenotype_STN2$sample <- gsub(pattern = '-',replacement = '.',x = Phenotype_STN2$sample)
 
@@ -104,29 +109,8 @@ Phenotype_Stage_All_PNintersect <- Phenotype_Stage_All[Phenotype_Stage_All$X_PAT
 GeneExp_Stage_PT2 <-  GeneExp_Stage_PT[,colnames(GeneExp_Stage_PT) %in% Phenotype_PT_fromGE_intersect$sample]
 GeneExp_Stage_STN2 <-  GeneExp_Stage_STN[,colnames(GeneExp_Stage_STN) %in% Phenotype_STN_fromGE_intersect$sample]
 
-###### Try ######
-# library("dplyr")
-# Phenotype_PT3 <- left_join(Phenotype_PT2,Phenotype_Stage_All,by="sample")
-# Phenotype_STN3 <- left_join(Phenotype_STN2,Phenotype_Stage_All,by="sample")
-# 
-# PSList <- as.data.frame(intersect(Phenotype_PT3$X_PATIENT,Phenotype_STN3$X_PATIENT))
-# #PSList <- intersect(Phenotype_PT3$X_PATIENT,Phenotype_STN3$X_PATIENT)
-# colnames(PSList) <-c("X_PATIENT")
-# 
-# PSList2 <- Phenotype_Stage_All[Phenotype_Stage_All$X_PATIENT %in% PSList$X_PATIENT,]
-# 
-# Phenotype_PT4 <- Phenotype_PT3[Phenotype_PT3$X_PATIENT %in% PSList2$X_PATIENT,]
-# Phenotype_STN4 <- Phenotype_STN3[Phenotype_STN3$X_PATIENT %in% PSList2$X_PATIENT,]
-# 
-# 
-# GeneExp_Stage_PT2 <-  GeneExp_Stage_PT[,colnames(GeneExp_Stage_PT) %in% Phenotype_PT4$sample]
-# GeneExp_Stage_STN2 <- GeneExp_Stage_STN[,colnames(GeneExp_Stage_STN) %in% Phenotype_STN4$sample]
 
-##########################################
-
-
-## Find Gene
-
+######## Find Gene
 GeneExp_RhoA_PT <- GeneExp_Stage_PT2[rownames(GeneExp_Stage_PT2) %in% c("RHOA","ROCK1","ROCK2","FN1"),]
 GeneExp_RhoA_PT[5,] <- colnames(GeneExp_RhoA_PT)
 GeneExp_RhoA_PT <- as.data.frame(t(GeneExp_RhoA_PT))
@@ -152,106 +136,54 @@ GeneExp_RhoA_All$ROCK2.y <- as.numeric(GeneExp_RhoA_All$ROCK2.y)
 GeneExp_RhoA_All$FN1.x <- as.numeric(GeneExp_RhoA_All$FN1.x)
 GeneExp_RhoA_All$FN1.y <- as.numeric(GeneExp_RhoA_All$FN1.y)
 
-# # V1
-#  GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > GeneExp_RhoA_All$RHOA.y, ]
-# 
-# # V2
-# GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > GeneExp_RhoA_All$RHOA.y &
-#                                                 GeneExp_RhoA_All$ROCK1.x > GeneExp_RhoA_All$ROCK1.y &
-#                                                 GeneExp_RhoA_All$ROCK2.x > GeneExp_RhoA_All$ROCK2.y &
-#                                                 GeneExp_RhoA_All$FN1.x > GeneExp_RhoA_All$FN1.y
-#                                                 ,]
-# # V3
-# GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > GeneExp_RhoA_All$RHOA.y &
-#                                                   GeneExp_RhoA_All$ROCK1.x > GeneExp_RhoA_All$ROCK1.y &
-#                                                   GeneExp_RhoA_All$ROCK2.x > GeneExp_RhoA_All$ROCK2.y
-#                                                 ,]
-# 
-# # V4
-# GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > GeneExp_RhoA_All$RHOA.y &
-#                                                   GeneExp_RhoA_All$ROCK1.x > GeneExp_RhoA_All$ROCK1.y
-#                                                 ,]
-# V5
-GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > GeneExp_RhoA_All$RHOA.y &
-                                                  GeneExp_RhoA_All$ROCK2.x > GeneExp_RhoA_All$ROCK2.y
-                                                ,]
 
-### Test ###
-# library("dplyr")
-# GeneExp_RhoA_2Group <- full_join(GeneExp_RhoA_PT,GeneExp_RhoA_STN,by="sample")
-# colnames(GeneExp_RhoA_2Group)[1] <- c("RhoA.PT")
-# colnames(GeneExp_RhoA_2Group)[1] <- c("RhoA.STN")
+###### Try different Combination ######
+# Comb1
+ GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > Fold_Change*GeneExp_RhoA_All$RHOA.y, ]
+
+# # Comb2
+# GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > Fold_Change*GeneExp_RhoA_All$RHOA.y &
+#                                                 GeneExp_RhoA_All$ROCK1.x > Fold_Change*GeneExp_RhoA_All$ROCK1.y &
+#                                                 GeneExp_RhoA_All$ROCK2.x > Fold_Change*GeneExp_RhoA_All$ROCK2.y &
+#                                                 GeneExp_RhoA_All$FN1.x > Fold_Change*GeneExp_RhoA_All$FN1.y
+#                                                 ,]
+# # Comb3
+# GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > Fold_Change*GeneExp_RhoA_All$RHOA.y &
+#                                                   GeneExp_RhoA_All$ROCK1.x > Fold_Change*GeneExp_RhoA_All$ROCK1.y &
+#                                                   GeneExp_RhoA_All$ROCK2.x > Fold_Change*GeneExp_RhoA_All$ROCK2.y
+#                                                 ,]
 # 
-# GeneExp_RhoA_All <- left_join(GeneExp_RhoA_2Group,Phenotype_Stage_All_PNintersect,by="sample")
-# # ttt <- intersect(colnames(GeneExp_Stage_PT2),colnames(GeneExp_Stage_STN2))
+# # Comb4
+# GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > Fold_Change*GeneExp_RhoA_All$RHOA.y &
+#                                                   GeneExp_RhoA_All$ROCK1.x > Fold_Change*GeneExp_RhoA_All$ROCK1.y
+#                                                 ,]
+# # Comb5
+# GeneExp_RhoA_All_Candidates <- GeneExp_RhoA_All[GeneExp_RhoA_All$RHOA.x > Fold_Change*GeneExp_RhoA_All$RHOA.y &
+#                                                   GeneExp_RhoA_All$ROCK2.x > Fold_Change*GeneExp_RhoA_All$ROCK2.y
+#                                                 ,]
+
 
 GeneExp_Stage_PT_Final <-  GeneExp_Stage_PT2[,colnames(GeneExp_Stage_PT2) %in% GeneExp_RhoA_All_Candidates$sample.x]
 GeneExp_Stage_STN_Final <-  GeneExp_Stage_STN2[,colnames(GeneExp_Stage_STN2) %in% GeneExp_RhoA_All_Candidates$sample.y]
 
-######## Old version #########
-# 
-# 
-# # load package 'data.table' 
-# library(data.table)
-# 
-# # Extract data with Target_gene_name
-# Target_gene <- GeneExp[Target_gene_name,]
-# 
-# # load package 'dplyr'
-# library(dplyr) # Basic data manupilation tools
-# Target_gene_Mean <- rowMeans(data.matrix(Target_gene))
-# # Target_gene_SD <- sd(data.matrix(Target_gene))
-# library(matrixStats)
-# Target_gene_SD <- rowSds(data.matrix(Target_gene))
-# 
-# # GeneExp_High <- GeneExp[,GeneExp[Target_gene_name,] >= Target_gene_Mean+Target_gene_SD]
-# # GeneExp_Low <- GeneExp[,GeneExp[Target_gene_name,] <= Target_gene_Mean-Target_gene_SD]
-# 
-# 
-# # Test OK
-# # TTT <- as.data.frame(Target_gene_Mean+Target_gene_SD)
-# # TTT2 <- as.data.frame(Target_gene_Mean-Target_gene_SD)
-# # GeneExp_High <- GeneExp[,GeneExp[Target_gene_name[1],] >= TTT[Target_gene_name[1],]]
-# # GeneExp_Low <- GeneExp[,GeneExp[Target_gene_name[1],] <= TTT2[Target_gene_name[1],]]
-# 
-# MpSD <- as.data.frame(Target_gene_Mean+Target_gene_SD)
-# MmSD <- as.data.frame(Target_gene_Mean-Target_gene_SD)
-# Mean <- as.data.frame(Target_gene_Mean)
-# 
-# # GeneExp_High <- GeneExp[,GeneExp[Target_gene_name[1],] >= MpSD[Target_gene_name[1],] 
-# #                         & GeneExp[Target_gene_name[2],] >= MpSD[Target_gene_name[2],] 
-# #                         & GeneExp[Target_gene_name[3],] >= MpSD[Target_gene_name[3],]
-# #                         & GeneExp[Target_gene_name[4],] >= MpSD[Target_gene_name[4],]]
-# # GeneExp_Low <- GeneExp[,GeneExp[Target_gene_name[1],] <= MmSD[Target_gene_name[1],]]
-# 
-# GeneExp_High <- GeneExp[,GeneExp[Target_gene_name[1],] >= Mean[Target_gene_name[1],] 
-#                         & GeneExp[Target_gene_name[2],] >= Mean[Target_gene_name[2],] 
-#                         & GeneExp[Target_gene_name[3],] >= Mean[Target_gene_name[3],]
-#                         & GeneExp[Target_gene_name[4],] >= Mean[Target_gene_name[4],]]
-# 
-# GeneExp_Low<- GeneExp[,GeneExp[Target_gene_name[1],] <= Mean[Target_gene_name[1],] 
-#                         & GeneExp[Target_gene_name[2],] <= Mean[Target_gene_name[2],] 
-#                         & GeneExp[Target_gene_name[3],] <= Mean[Target_gene_name[3],]
-#                         & GeneExp[Target_gene_name[4],] <= Mean[Target_gene_name[4],]]
-######## Old version #########
 
-GeneExp_High <- GeneExp_Stage_PT_Final 
-GeneExp_Low <- GeneExp_Stage_STN_Final 
+GeneExp_PT <- GeneExp_Stage_PT_Final 
+GeneExp_STN <- GeneExp_Stage_STN_Final 
 
 
-# Count the numbers
-GeneExp_High_Num <- length(GeneExp_High[1,])
-GeneExp_Low_Num <- length(GeneExp_Low[1,])
+###### Count the sample numbers ######
+GeneExp_PT_Num <- length(GeneExp_PT[1,])
+GeneExp_STN_Num <- length(GeneExp_STN[1,])
 GeneExp_Gene_Num <- length(GeneExp[,1])
-Sample_Num <- GeneExp_High_Num+GeneExp_Low_Num
+Sample_Num <- GeneExp_PT_Num+GeneExp_STN_Num
 
 
 
-GeneExp_High_Group <- matrix(c(0), nrow =1, ncol =GeneExp_High_Num)
-GeneExp_Low_Group <- matrix(c(1), nrow =1, ncol =GeneExp_Low_Num)
+GeneExp_PT_Group <- matrix(c(0), nrow =1, ncol =GeneExp_PT_Num)
+GeneExp_STN_Group <- matrix(c(1), nrow =1, ncol =GeneExp_STN_Num)
 
 
-GeneExp_Sum <- cbind(NAME=row.names(GeneExp),Description=matrix(c("na"), nrow =GeneExp_Gene_Num, ncol =1),GeneExp_High,GeneExp_Low)
+GeneExp_Sum <- cbind(NAME=row.names(GeneExp),Description=matrix(c("na"), nrow =GeneExp_Gene_Num, ncol =1),GeneExp_PT,GeneExp_STN)
 
 
 GSEA_GeneExp <- data.frame(t(colnames(GeneExp_Sum)), stringsAsFactors=FALSE)
@@ -261,20 +193,19 @@ colnames(GSEA_GeneExp) <- GSEA_GeneExp
 library("plyr")  
 GSEA_GeneExp <- rbind.fill(GSEA_GeneExp,GeneExp_Sum)
 
-#########################################
 
-
+###### Export GSEA expression table ######
 GSEASetting <- data.frame(NAME = c("#1.2",GeneExp_Gene_Num),Description = c('',Sample_Num))
 GSEA_GeneExp<-rbind.fill(GSEASetting,GSEA_GeneExp)
 
 write.table(GSEA_GeneExp,file=paste0(PathName,ResultFolderName,"/",FileName,"_",Target_gene_name_Multi,"_collapsed.csv"),quote = FALSE,row.names = FALSE,col.names = FALSE, na = "",sep = ',')
 write.table(GSEA_GeneExp,file=paste0(PathName,ResultFolderName,"/",FileName,"_",Target_gene_name_Multi,"_collapsed.gct"),quote = FALSE,row.names = FALSE,col.names = FALSE, na = "",sep = '\t')
-#########################################
 
-
+###### Export GSEA Phenotype info ######
 Pheno_Line1 <- c(Sample_Num,2,1)
 Pheno_Line2 <- c(paste0("#",Target_gene_name_Multi,"_PT"),paste0(Target_gene_name_Multi,"_STN"))
-Pheno_Line3 <- c(GeneExp_High_Group,GeneExp_Low_Group)
+Pheno_Line3 <- c(GeneExp_PT_Group,GeneExp_STN_Group)
 Pheno_sum <- rbind.fill(data.frame(t(Pheno_Line1)),data.frame(t(Pheno_Line2), stringsAsFactors=FALSE),data.frame(t(Pheno_Line3)))
 write.table(Pheno_sum,file=paste0(PathName,ResultFolderName,"/",FileName,"_",Target_gene_name_Multi,".cls"),quote = FALSE,row.names = FALSE, na = "",col.names = FALSE)
-#########################################
+
+###### Export parameter settings ######
